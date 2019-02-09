2f3c707519ccf84063fc0c62cb24fc759cd1cc2d
Bruce LANE <bruce@batchass.fr>
2019-02-09 11:59:41 +0100

    stars

--- src/composable-glsl-functions.js
+++ src/composable-glsl-functions.js
@@ -253,7 +253,47 @@ float _noise(vec3 v){
       return vec4(sin(length(_st-0.5)*10.0-time*speed));
     }
     `
-  },  
+  },
+  stars: {
+    type: 'src',
+    inputs: [
+      {
+        name: 'speed',
+        type: 'float',
+        default: 0.0
+      }
+    ],
+    glsl: `vec4 stars(vec2 _st, float speed) {
+      // https://www.shadertoy.com/view/lsf3z2
+      vec2 p = -1.0 + 2.0 *_st;
+      float M_PI = 3.141592653589793;
+      float M_2PI = 6.283185307179586;
+      float angle = atan(p.y, p.x);
+      float radius = sqrt(p.x*p.x + p.y*p.y);
+    
+      float n = speed;//9.0;
+      float angle_offset = 5.1*sin(0.3*time);
+      float k_amplitude = 0.9*cos(1.2*time);
+      float radius2 = radius + pow(radius, 2.0)*k_amplitude*sin(n*angle + angle_offset);
+      float width = 0.05;
+      float k_t = -0.04;
+      
+      float n_inv = 1.0 / float(n);
+      vec3 color;
+      float modulus = mod((radius2 + k_t*time) / width, 3.0);
+      if(modulus < 1.0) {
+        //color = vec3(0.5, 0.0, 0.8);
+        color = vec3(0.0, 0.14, 0.3);
+      } else if(modulus < 2.0) {
+        color = vec3(0.0, 0.0, 0.1);
+      } else {
+        color = vec3(0.2, 0.1, 0.0);
+      }
+      color /= 0.2 + pow(radius, 2.0);
+      return vec4(color, 1.0);
+    }
+    `
+  },
   hexler330: {
     type: 'src',
     inputs: [
