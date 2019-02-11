b714065e3c7e6b0c5052543f5c4d0c5267095497
Bruce LANE <bruce@batchass.fr>
2019-02-11 21:12:17 +0100

    blobs

--- src/composable-glsl-functions.js
+++ src/composable-glsl-functions.js
@@ -1525,6 +1525,95 @@ float _noise(vec3 v){
     }
     `
   },
+  map1: {
+    type: 'util',
+    glsl: `float map1(in vec3 p) {
+      // add repetition
+      vec3 q = mod( p + 2.0, 4.0) - 2.0;
+      //sphere
+      // no repeat float d1 = length( p ) - 1.0;
+      float d1 = length( q ) - 1.0;
+      // deform the sphere
+      d1 += 0.1 * sin( 10.0 * p.x + time );
+      //d1 += 0.1 * sin( 10.0 * p.x ) * sin( 10.0 * p.y + time ) * sin( 10.0 * p.z );
+
+      // add a floor (plane)
+      float d2 = p.y + 1.0;
+      // no blending
+      //return min( d1, d2 );	
+      // blending
+      float k = 0.20;
+      float h = clamp( 0.5 + 0.5 * ( d1 - d2 ) / k, 0.0, 1.0 );
+      return mix( d1, d2, h ) - k*h*(1.0-h);	
+    }
+    `
+  },
+  calcNormal: {
+    type: 'util',
+    glsl: `vec3 calcNormal( in vec3 pos ) {
+      vec3 e = vec3(0.0001,0.0,0.0);
+      vec3 nor;
+      // compute gradient
+      nor.x = map1(pos+e.xyy) - map1(pos-e.xyy);
+      nor.y = map1(pos+e.yxy) - map1(pos-e.yxy);
+      nor.z = map1(pos+e.yyx) - map1(pos-e.yyx);
+      return normalize(nor);
+    }
+    `
+  },
+  blobs: {
+    type: 'src',
+    inputs: [
+      {
+        name: 'speed',
+        type: 'float',
+        default: 0.0
+      }
+    ],
+    glsl: `vec4 blobs(vec2 _st, float speed) {
+      vec2 p = -1.0 + 2.0 *_st;
+      // fix aspect ratio
+      p.x *= resolution.x/resolution.y;
+      p.y = - p.y ;
+      // ro is where the camera is, the ray comes from here
+      vec3 ro = vec3( 0.0, 0.0, 2.0 );
+      // rd is the ray direction, look down
+      vec3 rd = normalize( vec3( p, - 1.0 ) );
+      
+      vec3 col = vec3( 0.0 );
+      
+      float tmax = 20.0;
+      float h = 1.0;
+      float t = 0.0;
+    
+      for( int i=0; i<100; i++ )
+      {
+        if ( h < 0.0001 || t>tmax ) break;
+        h = map1( ro + t*rd );
+        t += h;
+      }
+      vec3 lig = vec3( 0.5773 );
+      if ( t < tmax )
+      {
+        // create position for normal
+        vec3 pos = ro + t*rd;
+        // normal
+        vec3 nor = calcNormal( pos );
+        // white : col = vec3( 1.0 );
+        // light on x, y or z axis: col *= nor.y;
+    
+        col = vec3( 1.0, 0.8, 0.5 ) * clamp( dot( nor, lig ), 0.0, 1.0 );
+        // add blue ambient light from top nor.y
+        col += vec3( 0.2, 0.3, 0.199 ) * clamp( nor.y, 0.0, 1.0 );
+        // add constant lighting to fill the bottom of the sphere
+        col += 0.1;
+        // fog
+        col *= exp( -0.1 * t );
+      }
+      return vec4(col,1.0);
+    }
+    `
+  },
   circles: {
     type: 'src',
     inputs: [
